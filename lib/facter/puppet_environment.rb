b7f8ac42dbd8c62b0c2ff8b88cba529b91aa927e
tdockendorf <tdockendorf@60ccb607-682e-4238-bebe-9d451482b7c3>
2016-10-03 20:33:24 +0000

    Set puppet_environment fact from server provided variable - surprised getting environment for something like PuppetDB queries and mcollective filters is so difficult
    git-svn-id: https://svn.osc.edu/repos/osc-puppetmaster-conf/branches/test@1168 60ccb607-682e-4238-bebe-9d451482b7c3

--- lib/facter/puppet_environment.rb
+++
@@ -1,9 +0,0 @@
-require 'puppet'
-Facter.add('puppet_environment') do
-  setcode do
-    if ! Puppet.settings.global_defaults_initialized?
-      Puppet.settings.initialize_global_settings
-    end
-    Puppet[:environment].to_s
-  end
-end
